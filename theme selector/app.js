44a0c82177ea99647df5e17897a9b69601b2758a
zodwick <115868723+zodwick@users.noreply.github.com>
2023-01-05 00:24:58 +0530

    rainbow hue animation and other changes

--- theme selector/app.js
+++ theme selector/app.js
@@ -3,12 +3,25 @@ const lightButton=document.getElementById('light')
 const solarButton=document.getElementById('solar')
 const body=document.body
 
+
+
+const theme = localStorage.getItem('theme');
+const isSolar = localStorage.getItem("isSolar")
+
+if (theme) {
+    body.classList.add(theme);
+    isSolar && body.classList.add('solar');
+  }
+
 darkButton.onclick= () =>{
     body.classList.replace('light','dark');
+    localStorage.setItem('theme','dark');
+    
 }
 
 lightButton.onclick= () =>{
     body.classList.replace('dark','light');
+    localStorage.setItem('theme','light');
     
 }
 
@@ -17,12 +30,14 @@ solarButton.onclick= () =>{
         body.classList.remove('solar');
         solarButton.style.cssText= '--bg-solar:var(--yellow);'
         solarButton.innerText= 'solarize';
+        localStorage.removeItem('isSolar')
         
     }
     else{
         solarButton.style.cssText='--bg-solar:white;'
         body.classList.add('solar');
         solarButton.innerText = 'normalize';
-
+        localStorage.setItem('isSolar',true);
+        
     }
 }
