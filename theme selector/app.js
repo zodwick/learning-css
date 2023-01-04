cfad99b878b750aad8b6798cd035d240b25ac893
zodwick <115868723+zodwick@users.noreply.github.com>
2023-01-05 00:13:36 +0530

    theme seletor logic

---
+++ theme selector/app.js
@@ -0,0 +1,28 @@
+const darkButton=document.getElementById('dark')
+const lightButton=document.getElementById('light')
+const solarButton=document.getElementById('solar')
+const body=document.body
+
+darkButton.onclick= () =>{
+    body.classList.replace('light','dark');
+}
+
+lightButton.onclick= () =>{
+    body.classList.replace('dark','light');
+    
+}
+
+solarButton.onclick= () =>{
+    if(body.classList.contains('solar')){
+        body.classList.remove('solar');
+        solarButton.style.cssText= '--bg-solar:var(--yellow);'
+        solarButton.innerText= 'solarize';
+        
+    }
+    else{
+        solarButton.style.cssText='--bg-solar:white;'
+        body.classList.add('solar');
+        solarButton.innerText = 'normalize';
+
+    }
+}
