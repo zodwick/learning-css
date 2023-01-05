e925e866141c54969fe38bdd0ba9f8d378dfa91a
zodwick <115868723+zodwick@users.noreply.github.com>
2023-01-06 00:02:02 +0530

    navbar

---
+++ dropwdown_navbar_react/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <Navbar></Navbar>
+    );
+}
+
+function Navbar()
+{
+  return(
+<nav className='navbar'>
+  <ul className='navbar-nav'>
+
+  </ul>
+</nav>  )
+}
+
+export default App;
